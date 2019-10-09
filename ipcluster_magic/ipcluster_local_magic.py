140559769dda786ef78e1f567623ffcf090ec02d
Rafael Sarmiento <rafael.sarmiento@cscs.ch>
2019-10-09 15:12:44 +0200

    add local ipcluster support

---
+++ ipcluster_magic/ipcluster_local_magic.py
@@ -0,0 +1,111 @@
+import subprocess
+import os
+import pexpect
+import signal
+import time
+from IPython import get_ipython
+from IPython.core.magic import line_magic, magics_class, Magics
+from docopt import docopt, DocoptExit
+
+
+@magics_class
+class IPClusterMagics(Magics):
+    """engine an IPyParallel cluster.
+
+Usage:
+  %ipcluster start [options]
+  %ipcluster start [options] -m <modules>...
+  %ipcluster stop
+  %ipcluster (-h | --help)
+  %ipcluster --version
+
+Options:
+  -h --help                Show this screen.
+  -v --version             Show version.
+  -n --num_engines <int>   Number of engines (default 1 per node).
+  -m --modules <str>       Modules to load (default none).
+  -e --env <str>           Conda env to load (default none).
+  -d --dir <path>          Directory to engine engines (default $HOME)
+"""
+    def __init__(self, shell):
+        super().__init__(shell)
+        self.__version__ = '0.0.1'
+        self.running = False
+
+    def parse_args(self, line):
+        # Valid syntax
+        try:
+            args = docopt(self.__doc__,
+                          argv=line.split(),
+                          version=self.__version__)
+        # Invalid syntax
+        except DocoptExit:
+            print("Invalid syntax.")
+            print(self.__doc__)
+            return
+        # Other normal exit (--version)
+        except SystemExit:
+            args = {}
+
+        defaults = {
+            'start': None,
+            'stop': None,
+            '--modules': None,
+            '--env': None,
+        }
+
+        given = {key: val for key, val in args.items() if val}
+        combined = {**defaults, **given}
+
+        # Remove '--' from args
+        parsed_args = {key.replace('--', ''): val
+                       for key, val in combined.items()}
+
+        return parsed_args
+
+    def launch_engines(self, args):
+        self.controller = pexpect.spawn('ipcontroller --log-to-file')
+        # some a seconds need pass a after launching the ipcontroller
+        # before launching the ipengines. Otherwise it maigh happen
+        # that the controller doesn't notice that the engines have
+        # started.
+        time.sleep(3)
+        self.engines = [pexpect.spawn('ipengine --log-to-file')
+                        for i in range(int(args['num_engines']))]
+        for i in self.engines:
+            print('engine pid:', i.pid)
+
+        time.sleep(1)
+        print('ctrler pid:', self.controller.pid)
+
+        self.running = True
+
+    def stop_engines(self):
+        if self.running:
+            procs = [self.controller] + self.engines
+            for e in procs:
+                e.terminate()
+                # e.sendline('\003')
+                # time.sleep(2)
+                # e.kill(signal.SIGTERM)
+
+            self.running = False
+        else:
+            print("IPCluster not running.")
+
+    @line_magic
+    def ipcluster(self, line):
+        args = self.parse_args(line)
+
+        if not args:
+            return
+
+        if args['start']:
+            self.launch_engines(args)
+        elif args['stop']:
+            self.stop_engines()
+
+
+ip = get_ipython()
+ipcluster_magics = IPClusterMagics(ip)
+ip.register_magics(ipcluster_magics)
