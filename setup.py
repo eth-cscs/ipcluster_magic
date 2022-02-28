0d2445cc495c875f6228057ae082788612747b71
rafael <rafael.sarmiento@cscs.ch>
2022-02-28 11:04:13 +0100

    fixes for pypi packages

--- setup.py
+++ setup.py
@@ -7,14 +7,17 @@ version = {}
 with open(version_py) as fp:
     exec(fp.read(), version)
 
+with open("README.md", "r", encoding="utf-8") as fp:
+    long_description = fp.read()
 
-setup(name='ipcluster_magics',
+setup(name='ipcmagic-cscs',
       version=version['VERSION'],
       packages=['ipcmagic'],
       url='https://github.com/eth-cscs/ipcluster_magic',
-      license='BSD',
+      license='BSD 3-Clause',
       description='IPyParallel Slurm magics for jupyterlab at CSCS',
-      long_description=open('README.md').read(),
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       install_requires=[
           'ipython>=6.3.0',
           'pexpect>=4.8.0',
