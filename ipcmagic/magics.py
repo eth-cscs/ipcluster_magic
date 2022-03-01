959649a97bb819104fb7399aabfb47398e0f4e2c
Rafael Sarmiento <rafael.sarmiento@cscs.ch>
2022-03-01 18:49:37 +0100

    add mpirun support

--- ipcmagic/magics.py
+++ ipcmagic/magics.py
@@ -23,13 +23,17 @@ class IPClusterMagics(Magics):
       -h --help                Show this screen.
       -v --version             Show version.
       -n --num_engines <int>   Number of engines
-      -m --mpi                 Run with MPI support (engines are
-                               automatically distributed across nodes)
+      --launcher <str>         Job launcher (mpirun | srun | local)
     """
+
     def __init__(self, shell):
         super().__init__(shell)
         self.__version__ = VERSION
         self.running = False
+        self.launcher_np_opts = {
+            'srun': '-n',
+            'mpirun': '-np',
+        }
 
     def parse_args(self, line):
         # Valid syntax
@@ -50,7 +54,7 @@ class IPClusterMagics(Magics):
         defaults = {
             'start': None,
             'stop': None,
-            'mpi': False,
+            'launcher': 'srun'
         }
 
         given = {key: val for key, val in args.items() if val}
@@ -59,6 +63,13 @@ class IPClusterMagics(Magics):
         # Remove '--' from args
         parsed_args = {key.replace('--', ''): val
                        for key, val in combined.items()}
+
+        valid_launchers = ['mpirun', 'srun', 'local']
+        if parsed_args['launcher'] not in valid_launchers:
+            print(f"Invalid option for `--launcher`: "
+                  f"{parsed_args['launcher']} \n"
+                  f"Valid options are: {valid_launchers}")
+            return
 
         return parsed_args
 
@@ -104,8 +115,9 @@ class IPClusterMagics(Magics):
         # started.
         time.sleep(3)
         hostname = socket.gethostname()
+        np_opt = self.launcher_np_opts[self.args.launcher]
         self.engines = pexpect.spawn(
-            f'srun -n {self.args.num_engines} ipengine '
+            f'{self.args.launcher} {np_opt} {self.args.num_engines} ipengine '
             f'--location={hostname} --log-to-file')
         time.sleep(1)
         self.running = True
@@ -113,7 +125,7 @@ class IPClusterMagics(Magics):
 
     def launch_engines(self):
         if not self.running:
-            if self.args.mpi:
+            if self.args.launcher != 'local':
                 self._launch_engines_mpi()
             else:
                 self._launch_engines_local()
@@ -122,9 +134,9 @@ class IPClusterMagics(Magics):
 
     def stop_cluster(self):
         if self.running:
-            # `self.args['mpi']` is not valid for `ipcluster stop`.
+            # `self.args.launcher=local` is not valid for `ipcluster stop`.
             # If `[self.controller] + self.engines` can not be added
-            # it means that the `--mpi` option was used.
+            # it means that the local launcher was used.
             try:
                 procs = [self.controller] + self.engines
                 for e in procs:
